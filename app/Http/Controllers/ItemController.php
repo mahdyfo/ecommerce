2db9feb64b8e7940efe8f4bd5b5e579f0eda9b86
Mahdyfo <mahdyfof@gmail.com>
2020-11-30 15:24:03 +0330

    Created ItemController index method

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        return view('items.index');
+        $items['new'] = Item::getNewItems();
+
+        //Make it ready to be a VueJs prop
+        $items = json_encode($items);
+
+        return view('items.index', compact('items'));
     }
 }
