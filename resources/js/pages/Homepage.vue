cf11f7f9e5c906643404a3b0a47414d4c8b885f5
Mahdyfo <mahdyfof@gmail.com>
2020-11-30 14:24:40 +0330

    create item listing home page

---
+++ resources/js/pages/Homepage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="row">
+        <div class="col-md-9">
+            <items-box title="New Items"></items-box>
+        </div>
+        <div class="col-md-3">
+            <items-box title="Popular Items"></items-box>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["title"],
+
+        mounted() {
+
+        }
+    }
+</script>
