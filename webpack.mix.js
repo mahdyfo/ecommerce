30eb030c30477c29ce2b8abf082612cc3d1d3da4
Mahdyfo <mahdyfof@gmail.com>
2020-11-30 10:33:55 +0330

    Set up webpack (custom css file and versioning)

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,20 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+
+    // Compile SCSS file
+   .sass('resources/sass/app.scss', 'public/css')
+
+    // Add custom CSS file to the end
+    .styles(['public/css/app.css', 'resources/css/app.css'], 'public/css/app.css');
+
+// If in production, Version the file for caching purposes
+if (mix.inProduction()) {
+    mix.version();
+}
